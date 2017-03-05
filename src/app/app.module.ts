7359d4e3f2abf250bdd3c4d978183b376d0a67e2
anteAdamovic93 <ante.adamovic93@gmail.com>
2017-03-05 13:17:41 +0100

    Refactor imports for module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,40 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
 import { MyApp } from './app.component';
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
-import { LoginPage } from '../pages/login/login';
-import { RegisterPage } from '../pages/register/register';
+
+// Pages
+import { LoginPage } from '../pages/login/login.page';
+import { RegisterPage } from '../pages/register/register.page';
+import { HomePage } from '../pages/home/home.page';
+import { ErrorPage } from '../pages/error/error.page';
+
+// Services
 import { RouterService } from '../services/router.service';
 import { RegisterService } from '../services/register.service';
 import { LoginService } from '../services/login.service';
-import { Storage } from '@ionic/storage';
 
 @NgModule({
-  declarations: [
-    MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage,
-    LoginPage,
-    RegisterPage
-  ],
-  imports: [
-    IonicModule.forRoot(MyApp)
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage,
-    LoginPage,
-    RegisterPage
-  ],
-  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}, Storage, RouterService, RegisterService, LoginService ]
+    declarations: [
+        MyApp,
+        HomePage,
+        LoginPage,
+        RegisterPage,
+        ErrorPage
+    ],
+    imports: [
+        IonicModule.forRoot(MyApp)
+    ],
+    bootstrap: [IonicApp],
+    entryComponents: [
+        MyApp,
+        HomePage,
+        LoginPage,
+        RegisterPage,
+        ErrorPage
+    ],
+    providers: [
+        Storage, RouterService, RegisterService, LoginService,
+        { provide: ErrorHandler, useClass: IonicErrorHandler },]
 })
-export class AppModule {}
+export class AppModule { }
