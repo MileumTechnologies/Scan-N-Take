59623feee7ef0c0ece1e5f6983d27cbd265e8bac
anteAdamovic93 <ante.adamovic93@gmail.com>
2017-03-05 13:10:49 +0100

    Refactor RouterService due to NavController functionality.

--- src/services/router.service.ts
+++ src/services/router.service.ts
@@ -1,58 +1,13 @@
 import { Injectable, EventEmitter } from '@angular/core';
-import { NavController } from 'ionic-angular';
-
-import { LoginPage } from '../pages/login/login.page';
-import { RegisterPage } from '../pages/register/register.page';
-import { HomePage } from '../pages/home/home.page';
-import { UnauthorizedPage } from '../pages/unauthorized/unauthorized.page';
 
 @Injectable()
 export class RouterService {
     private changeRootEmitter: EventEmitter<any> = new EventEmitter();
 
-    constructor(private navigationController: NavController) {}
+    constructor() {}
 
-    /** 
-     *  @param Name of the page to be redirected to
-     *  
-     *  Push a new page to the navigation controller stack
-     */
-    // pushPage(newPage: string): void {
-
-    //     switch(newPage) {
-
-    //     }
-
-    // }
-
-    popPage(): void {
-        this.navigationController.pop();
-    }
-
-    changeRoot(newPage: string): void {
-
-        switch(newPage) {
-
-            case 'login': {
-                this.changeRootEmitter.emit(LoginPage);
-                break;
-            }
-
-            case 'register': {
-                this.changeRootEmitter.emit(RegisterPage);
-                break;
-            }
-
-            case 'home': {
-                this.changeRootEmitter.emit(HomePage);
-                break;
-            }
-
-            default: {
-                this.changeRootEmitter.emit(UnauthorizedPage);
-            }
-
-        }
+    changeRoot(newRoot: string): void {
+        this.changeRootEmitter.emit(newRoot);
     }
 
     getChangeRootEmitter(): EventEmitter<any> {
