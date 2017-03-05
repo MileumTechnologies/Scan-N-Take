18a858a95a0c1c1d87edcd3bd52ee6508fb1cb84
anteAdamovic93 <ante.adamovic93@gmail.com>
2017-03-05 11:52:08 +0100

    Renamed register -> register.page

--- src/pages/register/register.page.ts
+++ src/pages/register/register.page.ts
@@ -91,8 +91,8 @@ export class RegisterPage {
       });
   }
 
-  validateEmail(email) {
-    let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
+  validateEmail(email: string) {
+    let emailValidationTest = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return emailValidationTest.test(email);
   }
 }
