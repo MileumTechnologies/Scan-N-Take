7adbc81514d30fe888f959c18f6fbd9ba89858c6
anteAdamovic93 <ante.adamovic93@gmail.com>
2017-03-05 13:17:21 +0100

    Refactor app.component.ts for new nav controller and add ngOnDestroy.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,45 +1,70 @@
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
+import { Storage } from '@ionic/storage';
 
-import { TabsPage } from '../pages/tabs/tabs';
-import { LoginPage } from '../pages/login/login';
-import { RegisterPage } from '../pages/register/register';
+import { LoginPage } from '../pages/login/login.page';
+import { RegisterPage } from '../pages/register/register.page';
+import { HomePage } from '../pages/home/home.page';
+import { ErrorPage } from '../pages/error/error.page';
 import { RouterService } from '../services/router.service';
 
 @Component({
-  templateUrl: 'app.html'
+    templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage: any = LoginPage;
-  // rootPage: any = TabsPage;
+    rootPage: any = null;
 
-  constructor(platform: Platform, private router: RouterService) {
-    this.router.getRedirectEmitter().subscribe(
-      destination => this.redirect(destination)
-    );
+    constructor(platform: Platform, private routerService: RouterService, private storage: Storage) {
+        platform.ready().then(() => {
+            // Okay, so the platform is ready and our plugins are available.
+            // Here you can do any higher level native things you might need.
+            StatusBar.styleDefault();
 
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      StatusBar.styleDefault();
-      Splashscreen.hide();
-    });
-  }
+            // Check if user is already logged in
+            this.storage.get('id').then(
+                id => {
+                    if (id)
+                        this.changeRootPage('home');
+                    else
+                        this.changeRootPage('login');
 
-  redirect(destination: string) {
-    switch(destination) {
-      case 'login':
-        this.rootPage = LoginPage;
-        break;
-      case 'register':
-        this.rootPage = RegisterPage;
-        break;
-      case 'home':
-        this.rootPage = TabsPage;
-        break;
-      default:
-        break;
+                    Splashscreen.hide();
+                }
+            );
+
+        });
     }
-  }
+
+    ngOnInit() {
+        // Change root page subscription
+        this.routerService.getChangeRootEmitter().subscribe(
+            newRootPage => this.changeRootPage(newRootPage)
+        );
+    }
+
+    ngOnDestroy() {
+        this.routerService.getChangeRootEmitter().unsubscribe();
+    }
+
+    private changeRootPage(newRootPage: string): void {
+
+        switch (newRootPage) {
+            case 'login': {
+                this.rootPage = LoginPage;
+                break;
+            }
+            case 'register': {
+                this.rootPage = RegisterPage;
+                break;
+            }
+            case 'home': {
+                this.rootPage = HomePage;
+                break;
+            }
+            default: {
+                this.rootPage = ErrorPage;
+            }
+        }
+    }
 }
