212e2a02315c1b430044679597a8730fc23d0bd7
Ante <ante.adamovic93@gmail.com>
2017-03-31 04:23:17 +0200

    Rebase.

---
+++ src/pages/account/account.page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'account-page',
+	templateUrl: 'account.page.html'
+})
+
+export class AccountPage {
+	title: string = 'Account Page';
+}
---
+++ src/pages/help/help.page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'help-page',
+	templateUrl: 'help.page.html'
+})
+
+export class HelpPage {
+	title: string = 'Help Page';
+}
